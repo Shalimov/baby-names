d381d5013eb1bf44a6513f4c916b99a5a32459f6
Shalimov <Shalimich@gmail.com>
2018-10-07 15:52:52 +0300

    Add group resolver

--- lib/baby_names_web/graphql/resolvers/user/mutation.ex
+++ lib/baby_names_web/graphql/resolvers/user/mutation.ex
@@ -5,37 +5,38 @@ defmodule BabyNamesWeb.GraphQl.Resolvers.User.Mutation do
 
   alias BabyNames.Context.{Accounts, User}
 
+  def resolve_by(fun_name) do
+    fn params, resolution ->
+      user = Map.get(resolution.context, :current_user)
+      apply(__MODULE__, fun_name, [params, resolution, user])
+    end
+  end
+
   def create_user(params, _resolution) do
     Accounts.create_account(params)
   end
 
-  def create_collaboration(_params, %{context: context}) do
-    user = Map.get(context, :current_user)
+  def create_collaboration(_params, _, user) do
     User.create_collaboration(user.id)
   end
 
-  def connect(%{token: token}, %{context: context}) do
-    user = Map.get(context, :current_user)
+  def connect(%{token: token}, _, user) do
     User.connect_collaboration(token, user.id)
   end
 
-  def disconnect(%{token: token}, %{context: context}) do
-    user = Map.get(context, :current_user)
+  def disconnect(%{token: token}, _, user) do
     User.remove_collaboration(token, user.id)
   end
 
-  def mark_name_as_viewed(%{id: name_id}, %{context: context}) do
-    user = Map.get(context, :current_user)
+  def mark_name_as_viewed(%{id: name_id}, _, user) do
     User.marked_name_as_viewed(user.id, name_id)
   end
 
-  def remember_name(%{id: name_id}, %{context: context}) do
-    user = Map.get(context, :current_user)
+  def remember_name(%{id: name_id}, _, user) do
     User.marked_name_as_favourite(user.id, name_id)
   end
 
-  def forget_name(%{id: name_id}, %{context: context}) do
-    user = Map.get(context, :current_user)
+  def forget_name(%{id: name_id}, _, user) do
     User.remove_favourite_name(user.id, name_id)
   end
 end
--- lib/baby_names_web/graphql/types/user/mutation.ex
+++ lib/baby_names_web/graphql/types/user/mutation.ex
@@ -1,47 +1,47 @@
 defmodule BabyNamesWeb.GraphQl.Types.User.Mutation do
   use Absinthe.Schema.Notation
 
-  alias BabyNamesWeb.GraphQl.Resolvers
+  alias BabyNamesWeb.GraphQl.Resolvers.User
 
   object :user_mutations do
     field :create_user, :user do
       arg(:device_id, non_null(:id))
 
-      resolve(&Resolvers.User.Mutation.create_user/2)
+      resolve(&User.Mutation.create_user/2)
     end
 
     field :create_collaboration, :id do
-      resolve(&Resolvers.User.Mutation.create_collaboration/2)
+      resolve(User.Mutation.resolve_by(:create_collaboration))
     end
 
     field :connect, :boolean do
       arg(:token, non_null(:string))
 
-      resolve(&Resolvers.User.Mutation.connect/2)
+      resolve(User.Mutation.resolve_by(:connect))
     end
 
     field :disconnect, :boolean do
       arg(:token, non_null(:string))
 
-      resolve(&Resolvers.User.Mutation.disconnect/2)
+      resolve(User.Mutation.resolve_by(:disconnect))
     end
 
     field :mark_name_as_viewed, :boolean do
       arg(:id, non_null(:id))
 
-      resolve(&Resolvers.User.Mutation.mark_name_as_viewed/2)
+      resolve(User.Mutation.resolve_by(:mark_name_as_viewed))
     end
 
     field :remember_name, :boolean do
       arg(:id, non_null(:id))
 
-      resolve(&Resolvers.User.Mutation.remember_name/2)
+      resolve(User.Mutation.resolve_by(:remember_name))
     end
 
     field :forget_name, :boolean do
       arg(:id, non_null(:id))
 
-      resolve(&Resolvers.User.Mutation.forget_name/2)
+      resolve(User.Mutation.resolve_by(:forget_name))
     end
   end
 end
