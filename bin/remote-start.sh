e2fdc6c0d4f2d8285b405f8a59eae53ef811d2a6
Shalimov <Shalimich@gmail.com>
2018-12-10 10:36:35 +0300

    Update remote start

--- bin/remote-start.sh
+++ bin/remote-start.sh
@@ -10,13 +10,13 @@ export PORT=$PORT;
 echo \"PORT: \$PORT | DH: \$DB_HOST\";
 /opt/$APP_NAME_VSN/bin/$APP_NAME migrate &&\
 /opt/$APP_NAME_VSN/bin/$APP_NAME seeds &&\
-/opt/$APP_NAME_VSN/bin/$APP_NAME foreground;
+/opt/$APP_NAME_VSN/bin/$APP_NAME start;
 "
 
-log_success "Begin: Start remotely app in foreground"
+log_success "Begin: Start remotely app in background"
 
 ssh $RUSER@$RHOST "$REMOTE_CMD"
 
-log_success "End: Start remotely app in foreground"
+log_success "End: Start remotely app in background"
 
 exit 0
